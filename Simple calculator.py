2dd6429a55ffb23cd98361b09e90e9ce1439c49c
Alvaro105970 <Alvaro105970@mcpsva.org>
2026-09-23 14:32:35 -0400

    ....

--- Simple calculator.py
+++ Simple calculator.py
@@ -6,8 +6,9 @@ Instructor: Burgess
 """
 
 print("Welcome to the Simple Calculator")
-n1=int(input("enter your first number"))
-n2=int(input("enter your second number"))
-print(n1+n2)
-print(n1-n2)
-print("")
+n1=int(input("enter your first number :"))
+n2=int(input("enter your second number :"))
+print(f"{n1} + {n2} = {n1+n2}")
+print(f"{n1} - {n2} = {n1-n2}")
+print(f"{n1} * {n2} = {n1*n2}")
+print(f"{n1} / {n2} = {n1/n2}")
